76303fd8260a5af42beb084f18da11d659d2c07f
Hamza-Saif1 <80417663+Hamza-Saif1@users.noreply.github.com>
2021-04-15 04:34:33 -0700

    Lab Task1 and 2
    Movement of the ball script

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int speed = 3;
+    public Rigidbody2D rigidbody1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody1 = GetComponent<Rigidbody2D>();
+        Debug.Log("Start Gun");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mov = Input.GetAxis("Horizontal");
+        rigidbody1.velocity = new Vector3(mov * speed,rigidbody1.velocity.y,0);
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rigidbody1.velocity = new Vector2(rigidbody1.velocity.x, speed); // x-axis, y-axis, z-axis
+        }
+        Debug.Log(mov);
+    }
+}
